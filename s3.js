8be5b02b15ba9b8e66203543813c5ad78ef8e832
Dikshya  Bhandari <dikshyabhandari01@gmail.com>
2025-08-13 15:28:44 -0500

    first commit

---
+++ s3.js
@@ -0,0 +1,44 @@
+import aws from "aws-sdk";
+import dotenv from "dotenv";
+import crypto from "crypto";
+import { promisify } from "util";
+
+dotenv.config();
+
+const randomBytes = promisify(crypto.randomBytes);
+
+const region = process.env.AWS_REGION;
+console.log(region);
+const accessKeyID = process.env.AWS_ACCESSKEYID;
+console.log(accessKeyID);
+
+const secretAccessKey = process.env.AWS_SECRETACCESSKEYID;
+console.log(secretAccessKey);
+
+const bucketName = process.env.AWS_BUCKETNAME;
+console.log(bucketName);
+
+const s3 = new aws.S3({
+  region,
+  accessKeyID,
+  secretAccessKey,
+  signatureVersion: "v4",
+});
+console.log("heelo");
+console.log(s3);
+
+const generateSignedUrl = async () => {
+  const bytes = await randomBytes(16);
+  const imageName = bytes.toString("hex");
+  const params = {
+    Bucket: bucketName,
+    Key: imageName,
+    Expires: 60,
+  };
+  console.log(params);
+  const signedUrl = await s3.getSignedUrlPromise("putObject", params);
+  console.log(signedUrl);
+  return signedUrl;
+};
+
+export default generateSignedUrl;
